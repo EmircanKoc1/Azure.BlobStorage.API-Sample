81ff26f2d921264645d277cb8e72fd3f97df0a61
Emircan <emircandv787@gmail.com>
2024-12-16 05:51:30 +0300

    The AzureBlobOptions class has been added and appsettings.json file has been refactored.

---
+++ Azure.BlobStorage.API-Sample/Options/AzureBlobOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Azure.BlobStorage.API_Sample.Options
+{
+    public class AzureBlobOptions
+    {
+        public string DefaultContainerName { get; set; }
+        public string ConnectionString { get; set; }
+
+    }
+}
