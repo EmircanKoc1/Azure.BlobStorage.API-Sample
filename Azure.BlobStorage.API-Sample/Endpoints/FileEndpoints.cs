f237fe41848c09e9009979005250a36a3c456372
Emircan <emircandv787@gmail.com>
2024-12-16 05:53:44 +0300

    The IFileService interface is defined. The FileService class implements the IFileService interface.

---
+++ Azure.BlobStorage.API-Sample/Endpoints/FileEndpoints.cs
@@ -0,0 +1,75 @@
+﻿using Azure.BlobStorage.API_Sample.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Azure.BlobStorage.API_Sample.Endpoints;
+
+public static class FileEndpoints
+{
+
+    public static WebApplication AddFileEndpoints(this WebApplication app)
+    {
+        app.MapGet("file", async (
+            [FromServices] IFileService _fileService,
+            [FromQuery] string fileName,
+            [FromQuery] string? containerName) =>
+        {
+
+            var file = await _fileService.GetFileAsync(fileName, containerName: containerName);
+
+            if (file is null)
+                return Results.NotFound("The file was not found");
+            var memoryStream = new MemoryStream(file);
+            return Results.File(fileStream: memoryStream, fileDownloadName: fileName);
+
+
+        });
+        app.MapPost("file", async (
+            [FromServices] IFileService _fileService,
+            [FromQuery] string? containerName,
+            [FromForm] IFormFile file) =>
+        {
+
+
+
+            using (var stream = new MemoryStream())
+            {
+                await file.CopyToAsync(stream);
+
+                var response = await _fileService.UploadFileAsync(stream.ToArray(), file.FileName, containerName);
+
+                if (response)
+                    return Results.Ok("file uploaded");
+
+            }
+
+            return Results.BadRequest("File not uploaded");
+
+
+        }).DisableAntiforgery();
+
+
+        app.MapDelete("file", async (
+            [FromServices] IFileService _fileService,
+            [FromQuery] string containerName,
+            [FromQuery] string fileName) =>
+        {
+
+            return await _fileService.DeleteFileAsync(fileName, containerName)
+             ? Results.Ok("The file was deleted")
+             : Results.BadRequest();
+
+        });
+
+
+
+        return app;
+
+    }
+}
+
+
+
+
+
+
+
---
+++ Azure.BlobStorage.API-Sample/Services/IFileService.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Azure.BlobStorage.API_Sample.Services
+{
+    public interface IFileService
+    {
+
+        Task<bool> DeleteFileAsync(string fileName, [Optional] string containerName);
+        Task<bool> UploadFileAsync(byte[] file, string fileName, [Optional] string containerName);
+        Task<byte[]?> GetFileAsync(string fileName, [Optional] string containerName);
+
+
+    }
+}
