ec2dd048d98b526f18af1198e7fabd73715b3ef2
Emircan <emircandv787@gmail.com>
2024-12-16 05:55:23 +0300

    The services were registered to the IOC container , and the options pattern was implemented.

--- Azure.BlobStorage.API-Sample/Program.cs
+++ Azure.BlobStorage.API-Sample/Program.cs
@@ -1,20 +1,32 @@
+using Azure.BlobStorage.API_Sample.Endpoints;
+using Azure.BlobStorage.API_Sample.Options;
+using Azure.BlobStorage.API_Sample.Services;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 
+builder.Services.Configure<AzureBlobOptions>(builder.Configuration.GetSection("AzureBlobOptions"));
+
+builder.Services.AddSingleton<IFileService, FileService>();
+
+
+
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.MapScalarApiReference();
+    app.MapScalarApiReference(options =>
+    options.Theme = ScalarTheme.BluePlanet);
 }
 
 app.UseHttpsRedirection();
 
+app.AddFileEndpoints();
 
 
 await app.RunAsync();
